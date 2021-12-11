7b6855968c1dfba628821998d6f8f8880267562d
Katie Pundt <kzpundt@gmail.com>
2021-12-10 16:38:51 -0800

    add functionality to order bread, pastries, and get grand total

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -7,9 +7,9 @@ namespace PierresBakery
   {
     public static void Main()
     {
-      Console.WriteLine("- - - - - - - - - - - - - - -");
-      Console.WriteLine("Welcome to Pierre's Bakery!");
-      Console.WriteLine("- - - - - - - - - - - - - - -");
+      Console.WriteLine("- - - - - - - - - - - - - - - -");
+      Console.WriteLine(" Welcome to Pierre's Bakery!");
+      Console.WriteLine("- - - - - - - - - - - - - - - -");
       Console.WriteLine("Our prices:");
       Console.WriteLine("Bread: $5/ea");
       Console.WriteLine("Pastries: $2/ea");
@@ -18,12 +18,30 @@ namespace PierresBakery
       Console.WriteLine("Bread: Buy 2, get 1 free!");
       Console.WriteLine("Pastries: Buy 3 for $5!");
       Console.WriteLine("- - - - - - - - - - - - - - -");
-      Console.WriteLine("Would you like to place an order (Enter Y for yes, enter any other key to exit)?");
+      Console.WriteLine("Would you like to place an order? \n(Enter 'Y' for yes, enter any other key to exit)");
       string newOrder = Console.ReadLine();
 
       if (newOrder == "y" || newOrder == "Y")
       {
-        Console.WriteLine("Start order!");
+        Console.WriteLine("- - - - - - - - - - - - - - -");
+        Console.WriteLine("- - - - - New order - - - - -");
+        Console.WriteLine("- - - - - - - - - - - - - - -");
+        Console.WriteLine("How many loaves of bread would you like to order?");
+        string breadQtyStr = Console.ReadLine();
+        int breadQty = int.Parse(breadQtyStr);
+        Bread newBread = new Bread(breadQty);
+        int breadTotal = newBread.GetPrice();
+        Console.WriteLine("Bread Total: $" + breadTotal);
+        Console.WriteLine("- - - - - - - - - - - - - - -");
+        Console.WriteLine("How many pastries would you like to order?");
+        string pastryQtyStr = Console.ReadLine();
+        int pastryQty = int.Parse(pastryQtyStr);
+        Pastry newPastry = new Pastry(pastryQty);
+        int pastryTotal = newPastry.GetPrice();
+        Console.WriteLine("Pastry Total: $" + pastryTotal);
+        Console.WriteLine("- - - - - - - - - - - - - - -");
+        int grandTotal = breadTotal + pastryTotal;
+        Console.WriteLine("Order total: $" + grandTotal);
       }
       else 
       {
